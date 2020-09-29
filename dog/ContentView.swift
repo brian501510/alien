6036875b38048ae5030a507492a04404fb5f3a77
as468579 <as468579@gmail.com>
2020-09-29 22:35:59 +0800

    Initial Commit

---
+++ dog/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  dog
+//
+//  Created by User05 on 2020/9/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ dog/dogApp.swift
@@ -0,0 +1,17 @@
+//
+//  dogApp.swift
+//  dog
+//
+//  Created by User05 on 2020/9/29.
+//
+
+import SwiftUI
+
+@main
+struct dogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
